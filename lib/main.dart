e5a9fbcef175f42ccd50c7a959f30eb0c2348a06
Rafael Lima Felix <rafaellfx@gmail.com>
2020-09-14 22:59:07 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:animacaocomplexas/screens/login/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animations',
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
